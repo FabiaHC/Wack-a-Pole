1409e11e70e46fe3754b677ef03d1f55ebb88d15
Fabian <megaweepinbell@gmail.com>
2019-11-10 13:47:10 +0000

    Add initial Pygame code

--- src/main.py
+++ src/main.py
@@ -2,7 +2,25 @@ import pygame
 import random
 
 def main():
-    pass
-    
+    pygame.init()
+    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    x, y = screen.get_size()
+    done = False
+    pygame.display.set_caption('Wack a Pole!')
+    clock = pygame.time.Clock()
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+
+        pygame.display.update()
+        clock.tick(60)
+
+    print("END")
+
 if __name__ == "__main__":
     main()

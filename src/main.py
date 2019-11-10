a31599f0edbb7a8ff2ce76fc900650e3b620f920
Fabian <megaweepinbell@gmail.com>
2019-11-10 16:45:46 +0000

    Add MoleHole class

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 import pygame
 import random
 
+class MoleHole():
+    def __init__(self, screenX, screenY, number):
+        self.__width = screenX // 3 #Take up 1/3 of x width
+        self.__height = screenY // 3 #Take up 1/3 of y width
+        self.__x = ( (number)  % 3 ) * self.__width #x Blit Position
+        self.__y = ( (number) // 3 ) * self.__height #y Blit Position
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
@@ -8,6 +15,10 @@ def main():
     done = False
     pygame.display.set_caption('Wack a Pole!')
     clock = pygame.time.Clock()
+
+    moleHoles = []
+    for i in range(9):
+        moleHoles.append( MoleHole( x, y, i ) )
 
     while not done:
         for event in pygame.event.get():
